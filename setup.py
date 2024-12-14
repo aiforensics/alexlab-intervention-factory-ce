a0fe23aa7ff331f1ad8c9341e63fcd1032e4fd41
Riccardo Angius <694840+riccardoangius@users.noreply.github.com>
2024-12-14 02:26:28 +0100

    stopword package data

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from setuptools import setup
 setup(
     name="alexlab_user_actions",
     license='GPLv3',
-    packages=['alexlab_user_actions'],
+    packages=['alexlab_user_actions', 'alexlab_user_actions.stopwords'],
     description='Processes templates and arguments to generate user actions at scale (i.e. prompts for chatbot platforms such as Copilot and Gemini or search queries for TikTok and Youtube). ',
     author='AI Forensics',
     author_email='info@aiforensics.org',
+    package_dir={'alexlab_user_actions': 'alexlab_user_actions'},
+    package_data={'alexlab_user_actions': ['stopwords/*.txt']},
 )
